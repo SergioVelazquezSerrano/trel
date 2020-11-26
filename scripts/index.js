5c0bfc8c65c0469364a1dbbc81416f27e549365b
SergioVelazquezSerrano <72127059+SergioVelazquezSerrano@users.noreply.github.com>
2020-11-26 13:39:27 +0100

    parte I yataaa

--- scripts/index.js
+++ scripts/index.js
@@ -1,52 +1,55 @@
 
+
+function addDates(){
+    var cartas=[];
+    var dates =[];
+    var membreslist =[];
+    var cardsfin=[];
+    var cardsfecha=[];
+
+    
+    let dateIni=document.getElementById("fechIni").value;
+    let dateEnd=document.getElementById("fechEnd").value;
+    let dateEndsBool=document.getElementById("dataEndBool").checked;
+    /*let key=document.getElementById("key").value;
+    let token=document.getElementById("token").value;
+    let boardNo=document.getElementById("boardNo").value;
+    */
+    
 var key ="c42b569af23f3fb74bd843c9fdf476b8";
 var token = "40234d81e307fe1d361e1fc1668b72747dd7c23464202c4457c2799f8f836920";
 var boardNo = "sXTJhiXh";
-var ini = "2020-11-23";
-var fin = "2020-12-07";
-var cartas=[];
-var dates =[];
-var membreslist =[];
-var cardsfin=[];
-var cardsfecha=[];
+/*var dateIni = "2020-11-23";
+var dateEnd = "2020-12-07";
+var dateEndsBool = false;
+*/
 
-function addDates(){
-    let dateIni=document.getElementById("fechIni").value;
-    let dateEnd=document.getElementById("fechEnd").value;
-    let key=document.getElementById("key").value;
-    let token=document.getElementById("token").value;
-    let boardNo=document.getElementById("boardNo").value;
-    let dateEndsBool=document.getElementById("dataEndBool").checked;
-    //expresion regular para extraer el id de la url
-    
-    let id=new RegExp("a a");
-    console.log(dateIni);
-    console.log(dateEnd);
-    console.log(key);
-    console.log(token);
-    console.log(boardNo);
-    console.log(dateEndsBool);
-}
+    var cartas = getCards(cartas,boardNo,key,token);
+    var dates = getdate(dates,boardNo,key,token);
+    var membreslist = getMembers(membreslist,boardNo,key,token);
 
-function expres(){
-    var URL = "https://www.midominio.com/b/document/indice1.txt";
-    console.log(URL);
-    var re =/^(\w+):\/\/([^\/]+)([^]+)$/;
-    var resultado = re.exec(URL);
-    console.log(resultado[3]);
-    var exp=/^(\w+)([^\/]+)([^]+)$/;
-    var resp=re.exec(resultado[3]);
-    console.log(resp);
+    var cardsfin = modify(cartas,dates,membreslist);
+
+    console.log(cardsfin);
+
+
+    //parseo fechas o les doy fecha limite parseada
+    if(dateIni === ""){
+        dateIni=new Date("1989-09-10")
+    }else{
+        dateIni = new Date(dateIni);
+    }
+
+    if(dateEnd === ""){
+        dateEnd=new Date("2140-12-31")
+    }else{
+        dateEnd=new Date(dateEnd);
+    }
 
     
+    var cardsfecha = filter(cardsfecha,cardsfin,dateIni,dateEnd,dateEndsBool);
+    console.log(cardsfecha);
+    
+
 }
 
-var cartas = getCards(cartas,boardNo,key,token);
-var dates = getdate(dates,boardNo,key,token);
-var membreslist = getMembers(membreslist,boardNo,key,token);
-
-var cardsfin = modify(cartas,dates,membreslist);
-console.log(cardsfin);
-
-/*var cardsfecha = filter(cardsfecha,cardsfin,ini,fin);
-console.log(cardsfecha);*/
--- scripts/modify.js
+++ scripts/modify.js
@@ -25,20 +25,36 @@ function modify(cards, dates, membres){
 
 }
 
-function filter(cardsfecha,cardsfin,ini,fin){
+function filter(cardsfecha,cardsfin,dateIni,dateEnd,dateEndsBool){
 
     for( var i = 0; i<cardsfin.length ; i++){
-        if(cardsfin[i]["dateStart"].slice(0,10) != undefined){
-
+        
+        //convertir el dateStart de las cartas en formato date y asignacion sino tiene
+        if(cardsfin[i]["dateStart"].slice(0,10) != ""){
+            var ini = new Date(cardsfin[i]["dateStart"].slice(0,10));
+        }else{
+            var ini =  new Date("1989-09-10");
         }
-        var dateini = cardsfin[i]["dateStart"].slice(0,10);
-        var datefin = cardsfin[i]["dateEnd"].slice(0,10);
-        if(dateini>=ini && datefin<=fin){
-            cardsfin.push(cardsfin[i]);
+
+        //convertir el dateEnd de las cartas en formato date y asignacion sino tiene
+        if(cardsfin[i]["dateEnd"] != null){
+            var fin = new Date(cardsfin[i]["dateEnd"].slice(0,10));
+        }else{
+            var fin =  new Date("2140-12-31")
+        }
+
+        //comprobamos si quiere solo las finalizadas y sino sacamos todas que coincidan por fechas indicadas
+        if(dateEndsBool === true){
+            if(ini>=dateIni && fin<=dateEnd && cardsfin[i]["dateEndsBool"]===true){
+                cardsfecha.push(cardsfin[i]);
+            }
+        }else{
+            if(ini>=dateIni && fin<=dateEnd){
+                cardsfecha.push(cardsfin[i]);
+            }
         }
 
     }
-
 
     return cardsfecha;
 
