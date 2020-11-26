0bded328dd9f5984f5fe5e07756a4bc507e96806
Diego Chuquiguanga <diego@bcninfoservices.com>
2020-11-26 16:09:20 +0100

    modificacion

--- scripts/index.js
+++ scripts/index.js
@@ -1,66 +1,24 @@
-
-
 function addDates(){
-<<<<<<< HEAD
-//expresion regular para conexion, host y path
-    var re =/^(\w+):\/\/([^\/]+)([^]+)$/;
-//separador y limitador para separar el path    
-    var sep ="/",limit =4;
-
-=======
     var cartas=[];
     var dates =[];
     var membreslist =[];
     var cardsfin=[];
     var cardsfecha=[];
-
-<<<<<<< HEAD
-    
->>>>>>> 5c0bfc8c65c0469364a1dbbc81416f27e549365b
-=======
     //expresion regular para conexion, host y path
     var re =/^(\w+):\/\/([^\/]+)([^]+)$/;
     //separador y limitador para separar el path
     var sep ="/",limit =4;
-
->>>>>>> 926c7c1dd224f7e1df88083cfa4682d2a4cc4d2c
     let dateIni=document.getElementById("fechIni").value;
     let dateEnd=document.getElementById("fechEnd").value;
     let dateEndsBool=document.getElementById("dataEndBool").checked;
     /*let key=document.getElementById("key").value;
     let token=document.getElementById("token").value;
-<<<<<<< HEAD
-<<<<<<< HEAD
-    
-    let url=document.getElementById("boardNo").value;
-    //extraccion del id
-    var answer = re.exec(url);
-    var answer=answer[3].split(sep, limit);
-    let boardNo=answer[2];
-    let dateEndsBool=document.getElementById("dataEndBool").checked;
-    
-    console.log(dateIni);
-    console.log(dateEnd);
-    console.log(key);
-    console.log(token);
-    console.log(boardNo);
-    console.log(dateEndsBool);
-  
-}
-
-=======
-    let boardNo=document.getElementById("boardNo").value;
-=======
->>>>>>> 926c7c1dd224f7e1df88083cfa4682d2a4cc4d2c
     */
     let boardNo=document.getElementById("boardNo").value;
-
     //extraccion del id del tablero
     var answer = re.exec(boardNo);
     answer=answer[3].split(sep, limit);
     boardNo=answer[2];
-    console.log(boardNo);
-
 var key ="c42b569af23f3fb74bd843c9fdf476b8";
 var token = "40234d81e307fe1d361e1fc1668b72747dd7c23464202c4457c2799f8f836920";
 //var boardNo = "sXTJhiXh";
@@ -68,36 +26,23 @@ var token = "40234d81e307fe1d361e1fc1668b72747dd7c23464202c4457c2799f8f836920";
 var dateEnd = "2020-12-07";
 var dateEndsBool = false;
 */
-
     var cartas = getCards(cartas,boardNo,key,token);
     var dates = getdate(dates,boardNo,key,token);
     var membreslist = getMembers(membreslist,boardNo,key,token);
-
     var cardsfin = modify(cartas,dates,membreslist);
-
-    console.log(cardsfin);
->>>>>>> 5c0bfc8c65c0469364a1dbbc81416f27e549365b
-
-
+    //console.log(cardsfin);
     //parseo fechas o les doy fecha limite parseada
     if(dateIni === ""){
         dateIni=new Date("1989-09-10")
     }else{
         dateIni = new Date(dateIni);
     }
-
     if(dateEnd === ""){
         dateEnd=new Date("2140-12-31")
     }else{
         dateEnd=new Date(dateEnd);
     }
-
-    
     var cardsfecha = filter(cardsfecha,cardsfin,dateIni,dateEnd,dateEndsBool);
     console.log(cardsfecha);
-
-    //arrayObjToCsv(cardsfecha);
-    
-
+    arrayObjToCsv(cardsfecha);
 }
-
