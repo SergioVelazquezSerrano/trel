e3423872a341834dec2033d2b0810aed78a49a33
SergioVelazquezSerrano <72127059+SergioVelazquezSerrano@users.noreply.github.com>
2020-11-26 15:33:33 +0100

    empezando cvs

--- scripts/index.js
+++ scripts/index.js
@@ -7,18 +7,28 @@ function addDates(){
     var cardsfin=[];
     var cardsfecha=[];
 
-    
+    //expresion regular para conexion, host y path
+    var re =/^(\w+):\/\/([^\/]+)([^]+)$/;
+    //separador y limitador para separar el path
+    var sep ="/",limit =4;
+
     let dateIni=document.getElementById("fechIni").value;
     let dateEnd=document.getElementById("fechEnd").value;
     let dateEndsBool=document.getElementById("dataEndBool").checked;
     /*let key=document.getElementById("key").value;
     let token=document.getElementById("token").value;
-    let boardNo=document.getElementById("boardNo").value;
     */
-    
+    let boardNo=document.getElementById("boardNo").value;
+
+    //extraccion del id del tablero
+    var answer = re.exec(boardNo);
+    answer=answer[3].split(sep, limit);
+    boardNo=answer[2];
+    console.log(boardNo);
+
 var key ="c42b569af23f3fb74bd843c9fdf476b8";
 var token = "40234d81e307fe1d361e1fc1668b72747dd7c23464202c4457c2799f8f836920";
-var boardNo = "sXTJhiXh";
+//var boardNo = "sXTJhiXh";
 /*var dateIni = "2020-11-23";
 var dateEnd = "2020-12-07";
 var dateEndsBool = false;
@@ -49,6 +59,8 @@ var dateEndsBool = false;
     
     var cardsfecha = filter(cardsfecha,cardsfin,dateIni,dateEnd,dateEndsBool);
     console.log(cardsfecha);
+
+    //arrayObjToCsv(cardsfecha);
     
 
 }
